c3aa74bb844db0254f4773a5aa068dd7aa0540a7
Nikolas Chernyshev <nikolas.chernyshev@gmail.com>
2020-10-19 20:27:37 -0400

    Added random health calculator

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 console.log('A Monster approches');
 console.log('Prepare to Fight!');
 console.log('Choose your attack');
+
+class Character{
+
+constructor(){
+
+
+
+}
+Health(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    this.health= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+  } 
+
+}
+
+
+
