0b82f3939d510c24d6eaf3bba404d5c3a8af9922
Nikolas Chernyshev <nikolas.chernyshev@gmail.com>
2020-10-19 20:50:29 -0400

    added player and monster classes, added the slice,hack,and smash attacks for players, and added attack for monster

--- index.js
+++ index.js
@@ -9,18 +9,53 @@ constructor(){
 
 
 }
-Health(min, max) {
+  health(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      this.health= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+    } 
+  armourClass(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      this.armourClass= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+    } 
+
+}
+
+class player extends Character{
+  constructor(health,armourClass){
+    super(health);
+    super(armourClass);
+  }
+  slice(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    this.health= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+    this.slice= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
   } 
-armourClass(min, max) {
+  hack(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    this.health= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+    this.hack= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+  } 
+  smash(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    this.smash= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
   } 
 
 }
 
+class monster extends Character{
+  constructor(health,armourClass){
+    super(health);
+    super(armourClass);
+  }
+  attack(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    this.attack= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+  } 
 
 
+
+}
