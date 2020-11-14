48ef43f83d89c08eb73753978da126388eef4873
Nikolas Chernyshev <nikolas.chernyshev@gmail.com>
2020-11-14 18:53:47 -0500

    made user input possible, fixed character creation class, added attack input form user

--- index.js
+++ index.js
@@ -1,61 +1,55 @@
-console.log('A Monster approches');
-console.log('Prepare to Fight!');
-console.log('Choose your attack');
+const readlineSync = require("readline-sync");
+class Character {
+  constructor(healthMin, healthMax, armourClassMin, armourClassMax) {
+    this.health = this.randomIntInclusive(healthMin, healthMax);
 
-class Character{
-
-constructor(){
-
-
-
-}
-  health(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      this.health= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
-    } 
-  armourClass(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      this.armourClass= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
-    } 
-
-}
-
-class player extends Character{
-  constructor(health,armourClass){
-    super(health);
-    super(armourClass);
+    this.armourClass = this.randomIntInclusive(armourClassMin, armourClassMax);
   }
-  slice(min, max) {
+  randomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    this.slice= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
-  } 
-  hack(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    this.hack= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
-  } 
-  smash(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    this.smash= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
-  } 
-
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  }
 }
 
-class monster extends Character{
-  constructor(health,armourClass){
-    super(health);
-    super(armourClass);
+class Player extends Character {
+  constructor(
+    healthMin,
+    healthMax,
+    armourClassMin,
+    armourClassMax,
+    sliceMin,
+    sliceMax,
+    hackMin,
+    hackMax,
+    smashMin,
+    smashMax
+  ) {
+    super(healthMin, healthMax, armourClassMin, armourClassMax);
+    this.slice = this.randomIntInclusive(sliceMin, sliceMax);
+    this.hack = this.randomIntInclusive(hackMin, hackMax);
+    this.smash = this.randomIntInclusive(smashMin, smashMax);
   }
+}
+
+class Monster extends Character {
   attack(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    this.attack= Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
-  } 
+    this.attack = Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  }
+}
 
+const koz = new Player(15, 30, 10, 15, 13, 20, 1, 15, 25, 17, 28);
+console.log(koz);
 
+const cathulu = new Monster(50, 90, 40, 70);
+console.log(cathulu);
 
+console.log("A Monster approches");
+console.log("Prepare to Fight!");
+while (koz.health > 0 && cathulu.health > 0) {
+  let attackChosen = readlineSync.question("Choose your attack: ");
+  console.log(attackChosen);
+  break;
 }
